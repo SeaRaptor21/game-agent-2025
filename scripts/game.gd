1b88428cb088393531db3d15aaf2d3611b840667
SeaRaptor21 <elschuette21@gmail.com>
2025-04-03 17:39:50 -0700

    Restart with empty project

--- scripts/game.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-var player1 = null
-var player2 = null
-var finished = false
--- scripts/ui.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-func _process(_delta: float) -> void:
-	$MarginContainer/VBoxContainer/HBoxContainer/P1Health.value = Game.player1.health
-	$MarginContainer/VBoxContainer/HBoxContainer/P2Health.value = Game.player2.health
