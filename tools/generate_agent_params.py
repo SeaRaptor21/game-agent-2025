9381e93fe9e4fdd77054293ca9ff8257713ad99e
SeaRaptor21 <elschuette21@gmail.com>
2025-04-09 11:53:55 -0700

    Add comments to Python generator file

--- tools/generate_agent_params.py
+++ tools/generate_agent_params.py
@@ -1,4 +1,10 @@
-import numpy as np
+# See scripts/game.gd for an explanation of this file
+
+# Key:
+#  - # is a wall
+#  - ^ is a spike
+#  - G is the goal
+#  - < or > is wind
 
 board = [x.strip() for x in """
     #########
@@ -33,39 +39,44 @@ board = [x.strip() for x in """
     ##^   ^##
     ####G####
     #########
-""".strip().splitlines()]
-joined = "".join(board)
+""".strip().splitlines()] # Start with string representation, split into rows
+joined = "".join(board) # Join them all together to index for states
 width = len(board[0])
 height = len(board)
 states = width*height
 state_transitions = {}
 for s in range(states):
+    # Create the state transitions for each state
     if joined[s] == '#':
-        continue
+        continue # Ignore if it's a wall
     col = s % width
     row = s // width
-    #print(s, row, col)
     state_transitions[s] = [
-        s+width-1 if col>0 else s+width if row+1<height else s,
-        s+width if row+1<height else s,
-        s+width+1 if col+1<width else s+width if row+1<height else s
+        s+width-1 if col>0 else s+width if row+1<height else s, # Left and down if possible, then down, then nothing
+        s+width if row+1<height else s, # Down if possible, then nothing
+        s+width+1 if col+1<width else s+width if row+1<height else s # Down and right if possible, then down, then nothing
     ]
+    # Alternates for if the action would move us into a wall
     alt = s+width if joined[s+width] != '#' else s+width-1 if joined[s+width-1] != '#' else s+width+1 if joined[s+width+1] != '#' else s-1 if joined[s-1] != '#' else s+1 if joined[s+1] != '#' else s
+    # Use the alternate if we would otherwise be in a wall
     state_transitions[s] = [x if joined[x] != '#' else alt for x in state_transitions[s]]
     if joined[s] == '<':
+        # Just always go left if there's wind blowing left
         state_transitions[s] = [s+width-(1 if joined[s+width-1] != '#' else 0)]*3
     elif joined[s] == '>':
+        # Just always go right if there's wind blowing right
         state_transitions[s] = [s+width+(1 if joined[s+width-1] != '#' else 0)]*3
-rewards = [0.0]*states
-end_states = []
+rewards = [0.0]*states # Start with no rewards
+end_states = [] # Start with no end states
 for i, c in enumerate(joined):
-    if c in "L^":
+    if c == '^': # Spikes
         rewards[i] = -10.0
         end_states.append(i)
-    elif c == 'G':
+    elif c == 'G': # Goal
         rewards[i] = 10.0
         end_states.append(i)
 
+# Print out the values ready to be pasted into game.gd
 print("\n".join([x.strip() for x in """
     const STATES = {}
     const ACTIONS = {}
